d4cfbf8f0205ba41f91a9678dc5cb00669409d20
Leonardo Ribeiro <leonardo@ericom.com.br>
2022-07-15 10:33:38 -0300

    Toast

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { toast } from "react-toastify";
 import icon from "../../assets/img/notification-icon.svg";
 import { BASE_URL } from "../../utils/request";
 import "./styles.css";
@@ -7,7 +8,7 @@ type Props = {
 };
 function handleClick(id: number) {
   axios(`${BASE_URL}/sales/${id}/notification`).then((response) => {
-    console.log("sucesso");
+    toast.info("SMS inviado com sucesso!");
   });
 }
 function NotificationButton({ saleId }: Props) {
